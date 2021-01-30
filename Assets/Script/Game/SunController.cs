b4b9ab4df3163216cca577063f09c8baefd2f5e5
thcrack <kutar7580@gmail.com>
2021-01-30 19:33:36 +0800

    Added sun controller

--- Assets/Script/Game/SunController.cs
+++ Assets/Script/Game/SunController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Script.Game;
 using UnityEngine;
 
 public class SunController : MonoBehaviour
@@ -7,12 +9,21 @@ public class SunController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        OnBrightnessChanged(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnBrightnessChanged(float val)
     {
-        
+        transform.localScale = val * Vector3.one;
+    }
+
+    private void OnEnable()
+    {
+        GameEventManager.Instance.BrightnessChange += OnBrightnessChanged;
+    }
+
+    private void OnDisable()
+    {
+        GameEventManager.Instance.BrightnessChange -= OnBrightnessChanged;
     }
 }
