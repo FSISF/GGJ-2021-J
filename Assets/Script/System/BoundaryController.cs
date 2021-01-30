61a3afc6e91c596e472860d871eb524270103a02
akira2632 <akira2632@gmail.com>
2021-01-30 23:02:40 +0800

    BoundaryController加入Layer選項 因恐龍的移動範圍固定因此刪除不需要的EnableBoundary方法 修正恐龍可能從Floor邊緣掉落的問題

--- Assets/Script/System/BoundaryController.cs
+++ Assets/Script/System/BoundaryController.cs
@@ -6,51 +6,39 @@ namespace Script.System
 {
     public class BoundaryController : SingletonMono<BoundaryController>
     {
-        private GameObject _top, _bottom, _left, _right;
-        
+        [SerializeField] private BoxCollider2D floor;
+        [SerializeField] private string layerName;
+
         private void Start()
         {
             var verticalOffset = Camera.main.orthographicSize;
             var horizontalOffset = verticalOffset * Camera.main.aspect;
 
-            _top = CreateBound("TopBound",
+            CreateBound("TopBound",
                 new Vector3(0, verticalOffset + 0.5f),
                 new Vector2(horizontalOffset * 2 + 2, 1));
-            _bottom = CreateBound("BottomBound",
+            CreateBound("BottomBound",
                 new Vector3(0, -(verticalOffset + 0.5f)),
                 new Vector2(horizontalOffset * 2 + 2, 1));
-            _right = CreateBound("RightBound",
+            CreateBound("RightBound",
                 new Vector3(horizontalOffset + 0.5f, 0),
                 new Vector2(1, verticalOffset * 2 + 2));
-            _left = CreateBound("LeftBound",
+            CreateBound("LeftBound",
                 new Vector3(-(horizontalOffset + 0.5f), 0),
                 new Vector2(1, verticalOffset * 2 + 2));
+
+            floor.size = new Vector2(horizontalOffset * 2, floor.size.y);
         }
 
-        public void EnableLeftRightBound(bool enable)
-        {
-            _left.SetActive(enable);
-            _right.SetActive(enable);
-        }
-
-        public void EnableTopBound(bool enable)
-        {
-            _top.SetActive(enable);
-        }
-
-        public void EnableBottomBound(bool enable)
-        {
-            _bottom.SetActive(enable);
-        }
-        
-        private GameObject CreateBound(string objectName, Vector3 transformPosition, Vector2 boundSize)
+        private void CreateBound(string objectName, Vector3 transformPosition, Vector2 boundSize)
         {
             var result = new GameObject(objectName, typeof(BoxCollider2D));
             result.transform.SetParent(transform);
             result.transform.position = transformPosition;
+            layerName = "Player";
+            result.layer = LayerMask.NameToLayer(layerName);
             var bound = result.GetComponent<BoxCollider2D>();
             bound.size = boundSize;
-            return result;
         }
     }
 }
