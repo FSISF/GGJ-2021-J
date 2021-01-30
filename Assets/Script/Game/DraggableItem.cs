dbb5a35814aab47d8069378c599138c83f8a6d93
akira2632 <akira2632@gmail.com>
2021-01-30 16:14:24 +0800

    加入DraggableItem 新增PhysicMaterial 新增測試用場景Test2

---
+++ Assets/Script/Game/DraggableItem.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    [SerializeField, Tooltip("Ignore Gravity When Drag")]
+    private bool ignoreGravity = true;
+
+    private new Camera camera;
+    private float gravity;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var pointPosition = camera.ScreenToWorldPoint(eventData.position);
+        pointPosition.z = transform.position.z;
+        transform.position = pointPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (rigidbody is null) return;
+
+        gravity = rigidbody.gravityScale;
+        rigidbody.gravityScale = 0;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (rigidbody is null) return;
+
+        rigidbody.gravityScale = gravity;
+    }
+}
