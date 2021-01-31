824f5e3f10a62e034f7e3126425ee096f9db3618
akira2632 <akira2632@gmail.com>
2021-01-31 12:41:24 +0800

    場景修改 加入提示框 修復生命條完成

--- Assets/Script/RepairUI/RepairHealthPanel.cs
+++ Assets/Script/RepairUI/RepairHealthPanel.cs
@@ -1,40 +1,44 @@
+﻿using System;
 using Script.Game;
 using UnityEngine;
 
 namespace Script.RepairUI
 {
-    [RequireComponent(typeof(BoxCollider2D))]
-    public class RepairSettingIcon : MonoBehaviour
+    [RequireComponent(typeof(Collider2D))]
+    public class RepairHealthPanel : MonoBehaviour
     {
         [SerializeField] private string targetName;
-        [SerializeField] private GameObject settingButton;
+        [SerializeField] private GameObject healthPanel;
         [SerializeField] private GameObject tip;
 
         private bool _active;
 
         private void Start()
         {
-            GameEventManager.Instance.StartButtonOnHit += Rest;
-            settingButton.SetActive(false);
+            GameEventManager.Instance.BrightnessChange += OnBrightnessChange;
+            healthPanel.SetActive(false);
             tip.SetActive(false);
         }
 
-        private void Rest()
+        private void OnBrightnessChange(float value)
         {
+            if (value < 0.9f) return;
+
             _active = true;
             tip.SetActive(true);
+            GameEventManager.Instance.BrightnessChange -= OnBrightnessChange;
         }
+
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (!_active || other.transform.name != targetName) return;
-
-            GameEventManager.Instance.OnSettingIconBack();
-            settingButton.SetActive(true);
+            
+            GameEventManager.Instance.OnHealthPenalBack();
+            healthPanel.SetActive(true);
             tip.SetActive(false);
             Destroy(other.gameObject);
             _active = false;
         }
     }
 }
- 
